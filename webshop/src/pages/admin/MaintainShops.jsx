b652780529e8d535145bea8ac2570f6d43e1d7f4
gimmi74 <andero.sc@gmail.com>
2025-02-04 03:02:32 +0200

    Moved to repository

---
+++ webshop/src/pages/admin/MaintainShops.jsx
@@ -0,0 +1,12 @@
+
+function MaintainShops(
+  
+) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MaintainShops
---
+++ webshop/src/pages/auth/Login.jsx
@@ -0,0 +1,15 @@
+
+function Login() {
+  return (
+    <div>
+      <div>Login:</div> <br />
+      <div>Email:</div>
+      <input type="text" name="" id="" /> <br /> <br />
+      <div>Password:</div>
+      <input type="password" /> <br />
+      <button>Log In</button>
+    </div>
+  )
+}
+
+export default Login
